8207a2cd29f094869918625e35f913cc7571ecc4
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-08-19 11:54:23 +0600

    single find operation done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -34,6 +34,14 @@ async function run() {
       const craftData = craftCollection.find();
       const result = await craftData.toArray();
       res.send(result);
+    });
+
+    app.get("/craftData/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftCollection.findOne(query);
+      res.send(result);
+      console.log(query);
     });
 
     app.post("/craftData", async (req, res) => {
